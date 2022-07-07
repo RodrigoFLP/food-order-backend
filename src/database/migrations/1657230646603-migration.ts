ebd06a1beb845c8862be97ed46d3755d6e813fb8
Rodrigo López <rodrigolp0521@gmail.com>
2022-07-07 15:55:56 -0600

    add github action

--- src/database/migrations/1657230646603-migration.ts
+++ src/database/migrations/1657230646603-migration.ts
@@ -1,7 +1,7 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class migration1657214437850 implements MigrationInterface {
-    name = 'migration1657214437850'
+export class migration1657230646603 implements MigrationInterface {
+    name = 'migration1657230646603'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "customer" ALTER COLUMN "receiveAds" SET DEFAULT 'false'`);
