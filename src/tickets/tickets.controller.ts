de95c94d7b400122d0ea837b860a8f4099daba46
Rodrigo López <rodrigolp0521@gmail.com>
2022-08-17 17:54:09 -0600

    change method wompi-confirmation

--- src/tickets/tickets.controller.ts
+++ src/tickets/tickets.controller.ts
@@ -73,7 +73,7 @@ export class TicketsController {
   }
 
   @Public()
-  @Get('confirm-payment')
+  @Post('confirm-payment')
   confirmPayment(
     @Query('id') id: string,
     @Body() wompiWebhookBody: WompiWebhookBody,
